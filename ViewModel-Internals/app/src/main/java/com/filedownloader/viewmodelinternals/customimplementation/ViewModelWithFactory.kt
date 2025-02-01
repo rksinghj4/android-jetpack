993cf4b58fe067141caa3ea28e2ed9a7e14dc064
Raj Kumar Singh <rksinghj4@gmail.com>
2025-02-02 01:05:52 +0530

    Add ViewModel with Factory

---
+++ ViewModel-Internals/app/src/main/java/com/filedownloader/viewmodelinternals/customimplementation/ViewModelWithFactory.kt
@@ -0,0 +1,44 @@
+package com.filedownloader.viewmodelinternals.customimplementation
+
+import androidx.lifecycle.SavedStateHandle
+
+
+class ViewModelWithDependencies(
+    val savedStateHandle: SavedStateHandle
+) {
+    init {
+        println("ViewModelWithDependencies - initialize")
+    }
+
+    companion object {
+        fun create() {
+
+        }
+    }
+}
+
+fun interface Factory<T> {
+    fun create(): T
+}
+
+fun <T> getViewModel(factory: Factory<T>): T {
+    return factory.create()
+}
+
+private fun main() {
+    val vm1 = getViewModel {
+        ViewModelWithDependencies(SavedStateHandle())
+    }
+
+    val vm2 = getViewModel(
+        object : Factory<ViewModelWithDependencies> {
+            override fun create(): ViewModelWithDependencies {
+                return ViewModelWithDependencies(SavedStateHandle())
+            }
+        }
+    )
+
+    println("ViewModel 1= $vm1")
+    println("ViewModel 1= $vm2")
+}
+
